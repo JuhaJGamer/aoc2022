4c788cf8dcdaf11e8bd27400772552ea433f421c
kamray23 <juha@kamray.cf>
2022-12-06 13:07:47 +0200

    D5P2A1

--- day5.hs
+++ day5.hs
@@ -1,8 +1,9 @@
+{-# OPTIONS_GHC -Wno-deferred-out-of-scope-variables #-}
 module Day5 where
 
-import           Control.Applicative
 import qualified Data.Array    as Array
 import           Data.Array      (Array, array, (//), (!))
+import           Data.List
 import           Data.List.Split
 import           Data.Maybe
 
@@ -16,14 +17,12 @@ data Operation = Move Int Int Int
 type Procedure = [Operation]
 type Stack     = [Crate]
 
-transpose = getZipList . traverse ZipList
-
 readCrate :: String -> Maybe Crate
 readCrate ['[',c,']'] = Just $ Crate c
 readCrate _           = Nothing
 
 -- Reads *padded* (all same length) lines into stacks of crates
-readStacks :: [String] -> Array Int Stack 
+readStacks :: [String] -> Array Int Stack
 readStacks = toArray . map catMaybes . transpose . map readCrateLine
     where readCrateLine = map (readCrate . take 3) . chunksOf 4
           toArray xs = array (1, length xs) $ zip [1..] xs
@@ -39,7 +38,7 @@ padLines ss = map (padr len ' ') ss
 
 readOperation :: String -> Operation
 readOperation s
-  | head (words s) == "move" = 
+  | head (words s) == "move" =
       Move (read (words s!!1)) (read (words s!!3)) (read (words s!!5))
   | otherwise = undefined
 
@@ -47,13 +46,21 @@ performOp :: Operation -> Array Int Stack -> Array Int Stack
 performOp (Move 0 _ _) stacks = stacks
 performOp (Move n a b) stacks = performOp (Move (n-1) a b) newArr
     where crate  = head (stacks!a)
-          stack  = tail (stacks!a)
+          stack  = maybe [] snd (uncons (stacks!a))
           newArr = stacks // [(a,stack), (b, crate:(stacks!b))]
+
+performOp9001 :: Operation -> Array Int Stack -> Array Int Stack
+performOp9001 (Move n a b) stacks 
+  = let crates = take n (stacks!a)
+        stack  = drop n (stacks!a)
+     in stacks // [(a,stack), (b, crates ++ (stacks!b))]
 
 main = do input <- lines <$> getContents
           let (crateLines:procLines:_) = splitOn [""] input
               crates = readStacks $ padLines crateLines
               proc   = map readOperation procLines
-              finalCrates = foldr performOp crates proc
+              finalCrates = foldl' (flip performOp) crates proc
+              finalCrates9001 = foldl' (flip performOp9001) crates proc
           print $ map ((\(Crate c) -> c) . head) $ Array.elems finalCrates
+          print $ map ((\(Crate c) -> c) . head) $ Array.elems finalCrates9001
           putStrLn "trolled"
