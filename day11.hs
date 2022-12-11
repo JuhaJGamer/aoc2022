bed8c268b9ff29494a88c1fe68c9b331b7df46f8
kamray23 <juha@kamray.cf>
2022-12-11 12:13:33 +0200

    D11P2A1

--- day11.hs
+++ day11.hs
@@ -16,48 +16,65 @@ import System.IO
 
 type MonkeyId = Int
 type WorryLvl = Int
+data Test = Test { getMod :: Int
+                 , getT :: MonkeyId
+                 , getF :: MonkeyId
+                 }
+    deriving (Show, Eq)
 data Monkey = Monkey { getOp :: WorryLvl -> WorryLvl
-                     , getTest :: WorryLvl -> MonkeyId
+                     , getTest :: Test
                      }
 type Monkeys = [(MonkeyId,Monkey)]
 type Items   = Map MonkeyId [WorryLvl]
 type Round   = (Items, Map MonkeyId Int)
 
+type MonkeyProcess = (Monkey -> WorryLvl -> (MonkeyId, WorryLvl))
+type MonkeyProcess' = WorryLvl -> MonkeyProcess
+
 instance Show Monkey where
-    show m = "Monkey " ++ show (getOp m 1)
+    show m = "Monkey " ++ show (getOp m 1) ++ " " ++ show (getTest m)
 
 combineRounds :: Round -> Round -> Round
 combineRounds (_, nMap) (iMap, nMap') = (iMap, nMap'')
     where nMap'' = foldr (uncurry $ Map.insertWith (+)) nMap
                  $ Map.assocs nMap'
 
-processItem :: Monkey -> WorryLvl -> (MonkeyId, WorryLvl)
-processItem (Monkey op test) worry = (test newWorry, newWorry)
-    where newWorry = floor (fromIntegral (op worry) / 3)
+processItem :: WorryLvl -> MonkeyProcess
+processItem maxWorry (Monkey op (Test m t f)) worry = (test newWorry, newWorry)
+    where newWorry = floor (fromIntegral (op worry) / 3) `mod` maxWorry
+          test     = bool t f . (/= 0) . (`mod` m)
 
-monkeyRound :: Monkey -> [WorryLvl] -> [(MonkeyId, [WorryLvl])]
-monkeyRound monkey worry = collect $ map (processItem monkey) worry
+processItem2 :: WorryLvl -> MonkeyProcess
+processItem2 maxWorry (Monkey op (Test m t f)) worry = (test newWorry, newWorry)
+    where newWorry = floor (fromIntegral (op worry)) `mod` maxWorry
+          test     = bool t f . (/= 0) . (`mod` m)
+
+monkeyRound :: MonkeyProcess -> Monkey -> [WorryLvl] -> [(MonkeyId, [WorryLvl])]
+monkeyRound process monkey worry
+  = collect $ map (process monkey) worry
     where collect = foldr collect' [] . sortBy (comparing fst)
           collect' (i,x) [] = [(i,[x])]
           collect' (i,x) ((j,xs):is)
             | i == j = (i,x:xs):is
             | i /= j = (i,[x]):(j,xs):is
 
-fullRound :: Monkeys -> Items -> Round
-fullRound ms iMap = foldl (flip monkeyRound') (iMap, Map.empty) ms
+fullRound :: MonkeyProcess' -> Monkeys -> Items -> Round
+fullRound proc ms iMap = foldl (flip monkeyRound') (iMap, Map.empty) ms
     where monkeyRound' :: (MonkeyId, Monkey)
                        -> Round
                        -> Round
           monkeyRound' (mId,m) (iMap, nMap)
-            = let is'    = monkeyRound m (iMap!mId)
+            = let is'    = monkeyRound (proc maxWorry) m (iMap!mId)
                   iMap'  = foldr (uncurry (Map.insertWith (++))) iMap is'
                   iMap'' = Map.insert mId [] iMap'
                   nMap'  = Map.insert mId (length $ snd =<< is') nMap
                in (iMap'', nMap')
+          maxWorry :: Int
+          maxWorry = foldr (lcm . getMod . getTest . snd) 1 ms
 
-nRounds :: Monkeys -> Items -> Int -> Round
-nRounds ms iMap n
-  = foldl' (\r@(is,_) _ -> combineRounds r $ fullRound ms is) emptyRound [1..n]
+nRounds :: MonkeyProcess' -> Monkeys -> Items -> Int -> Round
+nRounds proc ms iMap n
+  = foldl' (\r@(is,_) _ -> combineRounds r $ fullRound proc ms is) emptyRound [1..n]
     where emptyRound = (iMap, Map.empty)
 
 parseMonkey :: [String] -> (MonkeyId, Monkey, [WorryLvl])
@@ -70,10 +87,10 @@ parseMonkey (idStr:isStr:opStr:pStrs) = (mId, m, is)
             | take 5 s == "old +" = (+(read $ drop 5 s))
             | take 5 s == "old *" = (*(read $ drop 5 s))
             | otherwise = error $ "Unexpected operation '" ++ s ++ "'"
-          testPred = (/= 0) . (`mod` (read $ drop 21 (head pStrs)))
+          testMod = read $ drop 21 (head pStrs)
           testOutcomes = (targetMonkey (pStrs!!1), targetMonkey (pStrs!!2))
           targetMonkey = read . (!!5) . splitOn " " . dropWhile isSpace
-          test = uncurry bool testOutcomes . testPred
+          test = uncurry (Test testMod) testOutcomes
           m = Monkey op test
 
 parseMonkeys :: [String] -> (Monkeys, Items)
@@ -86,16 +103,20 @@ parseMonkeys = first (sortOn fst)
                   -> (Monkeys, Items)
                   -> (Monkeys, Items)
 
+monkeyBusiness :: Round -> Int 
+monkeyBusiness = uncurry (*) . topTwo
+    where topTwo = (\[a,b] -> (a,b))
+                 . take 2
+                 . reverse
+                 . sort
+                 . map snd
+                 . Map.assocs
+                 . snd 
 
 main = do
     (ms, is) <- parseMonkeys . lines <$> getContents
-    let (_, inspections) =  nRounds ms is 20
-        topTwo = (\[a,b] -> (a,b))
-               . take 2 
-               . reverse
-               . sort 
-               . map snd 
-               $ Map.assocs inspections
-        monkeyBusiness = uncurry (*) topTwo
-    print monkeyBusiness
+    let rounds1 = nRounds processItem  ms is 20
+        rounds2 = nRounds processItem2 ms is 10000 
+    print $ monkeyBusiness rounds1
+    print $ monkeyBusiness rounds2
     putStrLn "trolled"
